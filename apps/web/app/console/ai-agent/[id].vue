fee7f7c8df83ee454fa2de9072b614567783b340
画上佳人纱 <2841541624@qq.com>
2025-08-15 12:12:00 +0800

    perf: 优化后台 UI

--- apps/web/app/console/ai-agent/[id].vue
+++ apps/web/app/console/ai-agent/[id].vue
@@ -47,7 +47,7 @@ watch(isMobile, (newVal) => {
 </script>
 
 <template>
-    <div class="flex h-full min-h-0 w-full">
+    <div class="flex h-[calc(100vh-6rem)] min-h-0 w-full">
         <div
             class="bg-muted flex h-full w-50 flex-none flex-col rounded-xl p-2"
             :class="{ '!w-18': collapsed }"
