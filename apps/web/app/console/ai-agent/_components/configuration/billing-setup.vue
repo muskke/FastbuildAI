61c980d4e3cdacb163123214ee5df75f990f73da
lucky <1538082851@qq.com>
2025-08-29 16:37:39 +0800

    perf: 优化智能体计费单位

--- apps/web/app/console/ai-agent/_components/configuration/billing-setup.vue
+++ apps/web/app/console/ai-agent/_components/configuration/billing-setup.vue
@@ -24,11 +24,12 @@ const { t } = useI18n();
                 type="number"
                 v-model="state.billingConfig!.price"
                 min="0"
-                :ui="{ base: 'pr-15' }"
+                :ui="{ base: 'pr-24' }"
                 @blur="if (state.billingConfig!.price < 0) state.billingConfig!.price = 0;"
             >
                 <template #trailing>
                     <div class="text-muted-foreground flex items-center justify-center gap-1">
+                        <span>{{ t("console-ai-provider.model.form.power") }}</span>
                         <span>/</span>
                         <span>{{ t("console-ai-agent.dashboard.times") }}</span>
                     </div>
