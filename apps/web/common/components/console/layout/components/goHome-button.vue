a178344ee2e307534eb65712c607b39afe3099b0
lucky <1538082851@qq.com>
2025-08-29 16:56:27 +0800

    feat: 控制台新增返回前台按钮

---
+++ apps/web/common/components/console/layout/components/goHome-button.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+/**
+ * 返回前台按钮组件
+ */
+
+const router = useRouter();
+
+/** 刷新页面 */
+function reloadPage() {
+    router.push("/");
+}
+</script>
+
+<template>
+    <div @click="reloadPage">
+        <UTooltip text="返回前台" :delay-duration="0">
+            <UButton
+                data-sidebar="trigger"
+                variant="ghost"
+                color="neutral"
+                size="xs"
+                :ui="{ base: 'py-2' }"
+            >
+                <span class="sr-only">Go Home</span>
+                <UIcon name="i-lucide-globe" class="size-4" />
+            </UButton>
+        </UTooltip>
+    </div>
+</template>
