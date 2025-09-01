6b99a0c0c55d313b09ce5594bb208c11e4952f5e
画上佳人纱 <2841541624@qq.com>
2025-09-01 15:06:29 +0800

    perf: 优化计费配置 UI

--- apps/web/app/console/ai-agent/_components/configuration/billing-setup.vue
+++ apps/web/app/console/ai-agent/_components/configuration/billing-setup.vue
@@ -15,28 +15,26 @@ const state = useVModel(props, "modelValue", emit);
 const { t } = useI18n();
 </script>
 <template>
-    <div>
-        <div class="flex w-full flex-col gap-2">
-            <div>
-                <span class="text-sm">{{
-                    t("console-ai-agent.configuration.dialogueConsumption")
-                }}</span>
-            </div>
-            <UInput
-                type="number"
-                v-model="state.billingConfig!.price"
-                min="0"
-                :ui="{ base: 'pr-24' }"
-                @blur="if (state.billingConfig!.price < 0) state.billingConfig!.price = 0;"
-            >
-                <template #trailing>
-                    <div class="text-muted-foreground flex items-center justify-center gap-1">
-                        <span>{{ t("console-ai-provider.model.form.power") }}</span>
-                        <span>/</span>
-                        <span>{{ t("console-ai-agent.dashboard.times") }}</span>
-                    </div>
-                </template>
-            </UInput>
+    <div class="bg-muted flex items-center justify-between rounded-lg p-3">
+        <div class="flex flex-col gap-1">
+            <span class="text-foreground text-sm font-medium">
+                {{ t("console-ai-agent.configuration.dialogueConsumption") }}
+            </span>
         </div>
+        <UInput
+            type="number"
+            v-model="state.billingConfig!.price"
+            min="0"
+            :ui="{ base: 'pr-24' }"
+            @blur="if (state.billingConfig!.price < 0) state.billingConfig!.price = 0;"
+        >
+            <template #trailing>
+                <div class="text-muted-foreground flex items-center justify-center gap-1">
+                    <span>{{ t("console-ai-provider.model.form.power") }}</span>
+                    <span>/</span>
+                    <span>{{ t("console-ai-agent.dashboard.times") }}</span>
+                </div>
+            </template>
+        </UInput>
     </div>
 </template>
