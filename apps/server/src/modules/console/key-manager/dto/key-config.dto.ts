778783de110d6dbcc7a8bb79c2984f2d5b1d5c38
JXDN <2749143951@qq.com>
2025-09-02 17:29:36 +0800

    feat: 允许特殊情况修改模板

--- apps/server/src/modules/console/key-manager/dto/key-config.dto.ts
+++ apps/server/src/modules/console/key-manager/dto/key-config.dto.ts
@@ -101,9 +101,7 @@ export class CreateKeyConfigDto {
  * 更新密钥配置DTO
  * 排除了 templateId 字段，更新时不允许修改所属模板
  */
-export class UpdateKeyConfigDto extends PartialType(
-    OmitType(CreateKeyConfigDto, ["templateId"] as const),
-) {}
+export class UpdateKeyConfigDto extends PartialType(CreateKeyConfigDto) {}
 
 /**
  * 查询密钥配置DTO
