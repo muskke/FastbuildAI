992c4f83f494c858eaaa8fceb055515acbc33ac8
JXDN <2749143951@qq.com>
2025-11-12 18:10:32 +0800

    fix: 修复插件跳转问题

--- extensions/buildingai-simple-blog/src/web/pages/index.vue
+++ extensions/buildingai-simple-blog/src/web/pages/index.vue
@@ -4,7 +4,7 @@ import { apiGetPublishedArticles } from "~/services/web/article";
 import type { Category } from "~/services/web/category";
 import { apiGetCategoryList } from "~/services/web/category";
 
-definePageMeta({ auth: false });
+definePageMeta({ auth: false, name: "文章中心", inLinkSelector: true });
 
 const { t } = useI18n();
 
