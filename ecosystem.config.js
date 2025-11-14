c4c150253b5b02408492e8f512df9e2ff74dae7a
JXDN <2749143951@qq.com>
2025-11-14 15:08:27 +0800

    ci: 修改宝塔面板部署pm2日志位置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,9 +18,9 @@ module.exports = {
             env_development: {
                 NODE_ENV: "development",
             },
-            error_file: "logs/pm2/api-error.log",
-            out_file: "logs/pm2/api-out.log",
-            log_file: "logs/pm2/api-combined.log",
+            error_file: "../../logs/pm2/api-error.log",
+            out_file: "../../logs/pm2/api-out.log",
+            log_file: "../../logs/pm2/api-combined.log",
             time: true,
             merge_logs: true,
             log_date_format: "YYYY-MM-DD HH:mm:ss Z",
